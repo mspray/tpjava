f18921a04e5f022c0934fca957c66ced03adce80
Olivier <olallinec@gmail.com>
2020-06-02 15:14:28 +0200

    Ajout des Exceptions

--- classroom/src/classroom/Date.java
+++ classroom/src/classroom/Date.java
@@ -2,34 +2,48 @@ package classroom;
 
 
 public class Date {
-	protected int jour;
-	protected int mois;
-	protected int annee;
-	
-	
-	public Date()
-	{
-		this.jour = 0;
-		this.mois = 0;
+
+	public int annee;
+	public int mois;
+	public int jour;
+
+	public Date (){
 		this.annee = 0;
+		this.mois = 0;
+		this.jour = 0;
 	}
-	public Date(int jour, int mois, int annee)
-	{
-		this.jour = jour;
-		this.mois = mois;
-		this.annee = annee;
+	public Date(int Jour, int Mois, int Annee) throws DateException{
+		this.annee = Annee;
+		if (Mois < 0 || Mois > 12){
+			throw new DateException();
+		}
+		this.mois = Mois;
+		if (Jour < 0 || Jour > 31){
+			throw new DateException();
+		}
+		if(Jour == 31 && (Mois == 2 || Mois == 4 || Mois == 6 || Mois == 9 || Mois == 11)){
+			throw new DateException();
+		} else if (Jour == 30 && Mois == 2){
+			throw new DateException();
+		} else if (Jour == 29  && Mois == 2 && Annee%4 != 0){
+			throw new DateException();
+		}
+		this.jour = Jour;
 	}
+
 	public Date (Date d)
 	{
 		this.jour = d.jour;
 		this.mois = d.mois;
 		this.annee = d.annee;
 	}
-	
+
+	@Override
 	public String toString() {
 		return""+this.annee + "."+this.mois+"."+this.jour;
 	}
 }
+
 	
 	
 	
--- classroom/src/classroom/DateHeure.java
+++ classroom/src/classroom/DateHeure.java
@@ -4,24 +4,26 @@ public class DateHeure extends Date{
 	private int heure;
 	private int minute;
 
-public DateHeure()  {
-	this.heure = 0;
-	this.minute = 0;
-}
+	public DateHeure()  {
+		super();
+		this.heure = 0;
+		this.minute = 0;
+	}
 
-public DateHeure (int jour, int mois, int annee, int heure, int minute) {
-	super.jour = jour;
-	super.mois = mois;
-	super.annee = annee;
-	this.heure = heure;
-	this.minute = minute;
-}
-public DateHeure (DateHeure d) {
-	this.heure = d.heure;
-	this.minute = d.minute;
-	
-}
-public String toString() {
-	return"" +super.annee +"."+ super.mois  +"."+super.jour +"."+ this.heure + "h"+this.minute;
-}
+	public DateHeure (int jour, int mois, int annee, int heure, int minute) throws DateException, HeureException {
+		super(jour,mois,annee);
+		if(heure < 0 || heure > 24 || minute < 0 || minute > 60){
+			throw new HeureException();
+		}
+		this.heure = heure;
+		this.minute = minute;
+	}
+	public DateHeure (DateHeure d) {
+		this.heure = d.heure;
+		this.minute = d.minute;
+
+	}
+	public String toString() {
+		return"" +super.annee +"."+ super.mois  +"."+super.jour +"."+ this.heure + "h"+this.minute;
+	}
 }
