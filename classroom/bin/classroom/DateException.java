a7fef4dff1d5539a94ea147e0964879f5eb28cb2
Olivier <olallinec@gmail.com>
2020-06-02 15:02:04 +0200

    Ajout des Exceptions

---
+++ classroom/bin/classroom/DateException.java
@@ -0,0 +1,7 @@
+package classroom;
+
+public class DateException extends Exception {
+    public DateException(){
+        System.out.println("Date incorrecte");
+    }
+}
---
+++ classroom/bin/classroom/HeureException.java
@@ -0,0 +1,8 @@
+package classroom;
+
+public class HeureException extends Exception{
+    public HeureException(){
+        System.out.println("Heure incorrecte");
+    }
+}
+
