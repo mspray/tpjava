347476c0042a0b309309d85b532db872458bf5f7
mspray <alexis@mairena.fr>
2020-06-15 22:53:46 +0200

    add save file + menu filechooser

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,35 +1,28 @@
 package sample;
 
-import com.sun.deploy.util.StringUtils;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.input.Clipboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.web.HTMLEditor;
 
-import java.awt.Robot;
-import java.awt.MouseInfo;
-
-import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javafx.fxml.FXML;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.web.WebView;
-import javafx.scene.web.WebEngine;
 import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 
 public class Controller implements Initializable {
@@ -46,6 +39,12 @@ public class Controller implements Initializable {
     @FXML
     private WebView textdst;
 
+    @FXML
+    private MenuBar vbMenu;
+
+
+    @FXML
+    private MenuItem menusave;
 
     @FXML
     private Button btn;
@@ -55,6 +54,10 @@ public class Controller implements Initializable {
 
     @FXML
     private Label textsrcnbl;
+
+    @FXML
+    FileChooser fileChooser = new FileChooser();
+
 
     @FXML
     void calculer_nombrecrc(KeyEvent event) {
@@ -103,24 +106,80 @@ public class Controller implements Initializable {
     }
 
     public void ouvrir(ActionEvent event) {
-
     }
     public void coller(ActionEvent event) {
         Clipboard systemClipboard = Clipboard.getSystemClipboard();
         String clipboardText = systemClipboard.getString();
         //Cursor p = getCursor();
-
+        Cursor p = textsrc.getCursor();
+        textsrc.setCursor(p);
+        textsrc.cursorProperty().setValue(p); // Trouver un moyen pour coller a la place exacte du curseur actuel.
         textsrc.setText(clipboardText);
-        //textsrc.setCursor(p).setText()
     }
 
+    private void saveFile(String wording, File file){
+        try {
+            PrintWriter write;
+            write = new PrintWriter(file);
+            write.println(wording);
+            write.close();
+        } catch(IOException ex){
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void handlesave(ActionEvent event){
+
+
+        Window stage = vbMenu.getScene().getWindow();
+
+        fileChooser.setInitialDirectory(new File("C:\\Users\\AM\\IdeaProjects\\testjavafx\\src\\sample")); // go to initialize
+
+        fileChooser.setTitle("Enregistrer un fichier dans le stockage local");
+        String defaultSaveName = "sauvegarde";
+        fileChooser.setInitialFileName(defaultSaveName);
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter(".html", "*.html"),
+                new FileChooser.ExtensionFilter(".txt", "*.txt"));
+        try
+        {
+            File file = fileChooser.showSaveDialog(stage);
+            if (file != null) {
+                File dir = file.getParentFile();
+                fileChooser.setInitialDirectory(dir); // enregistrer le repertoire pour la prochaine fois.
+                final String text_to_save = textsrc.getText();
+                saveFile(text_to_save, file);
+
+            }
+
+        }
+        catch (Exception ex){}
+
+        String content;
+        content = textsrc.getText();
+
+
+    }
+
+    public void handleopen(ActionEvent event){
+        Window stage = vbMenu.getScene().getWindow();
+        fileChooser.setInitialDirectory(new File("C:\\Users\\AM\\IdeaProjects\\testjavafx\\src\\sample")); // go to initialize
+
+        fileChooser.setTitle("Ouvrir un fichier");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("text file", "*.txt"),
+                new FileChooser.ExtensionFilter("html", "*.html"));
+
+        try
+        {
+            File file = fileChooser.showOpenDialog(stage);
+            fileChooser.setInitialDirectory(file.getParentFile()); // enregistrer le repertoire pour la prochaine fois.
+        }
+        catch (Exception ex){}
 
 
 
 
-
-
-
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -128,7 +187,6 @@ public class Controller implements Initializable {
         //WebEngine render = textdst.getEngine();
         //render.loadContent(String.valueOf(textsrc));
         //we.loadContent("http://google.fr");
-
 
     }
 
